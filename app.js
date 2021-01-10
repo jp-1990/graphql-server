e1f2ea631c4e85fac900e86da23116bb4b1898fd
jp-1990 <plum_1990@hotmail.co.uk>
2021-01-10 16:40:06 +0000

    update implemented CRUD operations

--- app.js
+++ app.js
@@ -12,6 +12,7 @@ const db = mysql.createConnection({
   database: 'testdb',
 });
 
+// -- RESOLVERS -- //
 // get all clients
 const getClients = (context) =>
   new Promise((resolve, reject) => {
@@ -33,7 +34,92 @@ const getClients = (context) =>
     });
   });
 
-const clients = [];
+// get client by id
+const getClient = (args, context) =>
+  new Promise((resolve, reject) => {
+    context.db.query(
+      `SELECT * FROM clients WHERE id = ${args.client.id};`,
+      (err, res, _) => {
+        if (err) {
+          console.log(err);
+          return reject(err);
+        }
+        // return client details
+        return resolve({
+          id: args.client.id,
+          name: res[0].name,
+          email: res[0].email,
+          phone: res[0].phone,
+        });
+      }
+    );
+  });
+
+// create client
+const createClient = (args, context) =>
+  new Promise((resolve, reject) => {
+    context.db.query(
+      `INSERT INTO clients (name, email, phone) VALUES ("${args.clientInput.name}", "${args.clientInput.email}", "${args.clientInput.phone}");`,
+      (err, res, _) => {
+        if (err) {
+          console.log(err);
+          return reject(err);
+        }
+        // return client details
+        return resolve({
+          id: res.insertId,
+          name: args.clientInput.name,
+          email: args.clientInput.email,
+          phone: args.clientInput.phone,
+        });
+      }
+    );
+  });
+
+// update client by id
+const updateClient = (args, context) =>
+  new Promise((resolve, reject) => {
+    console.log('no early exit');
+    context.db.query(
+      `UPDATE clients SET ${
+        args.client.name ? `name = "${args.client.name}"` : null
+      } WHERE id=${args.client.id};`,
+      (err, res, _) => {
+        //console.log(res);
+        if (err) {
+          console.log(err);
+          return reject(err);
+        }
+        // return client details
+        return resolve(args.client.id);
+      }
+    );
+  });
+
+// delete client by id
+const deleteClient = (args, context) =>
+  new Promise((resolve, reject) => {
+    context.db.query(
+      `DELETE FROM clients WHERE id=${args.deleteID};`,
+      (err, res, _) => {
+        // if error, reject promise and log error
+        if (err) {
+          console.log(err);
+          return reject(err);
+        }
+        // if nothing is deleted
+        if (res.affectedRows !== 0) {
+          return resolve({
+            id: `Client ID:${args.deleteID} successfully deleted`,
+          });
+        }
+        // if delete is successful
+        return resolve({
+          id: `Delete failed for Client ID:${args.deleteID}. Entry may not exist.`,
+        });
+      }
+    );
+  });
 
 const schema = buildSchema(`
   type Client {
@@ -47,13 +133,25 @@ const schema = buildSchema(`
     email: String!
     phone: String!
   }
+  input ClientUpdateInput {
+    id: Int!
+    name: String
+    email: String
+    phone: String
+  }
+  type DeletedClient{
+    id: String!
+  }
 
   type RootQuery {
     clients: [Client!]!
   }
   type RootMutation {
     createClient(clientInput: ClientInput): Client
+    deleteClient(deleteID: Int): DeletedClient
+    updateClient(client: ClientUpdateInput): Client
   }
+ 
   schema {
     query: RootQuery
     mutation: RootMutation
@@ -61,18 +159,20 @@ const schema = buildSchema(`
 `);
 
 const rootValue = {
-  clients: async (args, context) => {
+  clients: async (_, context) => {
     return await getClients(context);
   },
-  createClient: (args) => {
-    const client = {
-      id: Math.floor(Math.random() * 100).toString(),
-      name: args.clientInput.name,
-      email: args.clientInput.email,
-      phone: args.clientInput.phone,
-    };
-    clients.push(client);
-    return client;
+  createClient: async (args, context) => {
+    return await createClient(args, context);
+  },
+  deleteClient: async (args, context) => {
+    return await deleteClient(args, context);
+  },
+  updateClient: async (args, context) => {
+    if (!args.client.name && !args.client.email && !args.client.phone)
+      return { id: args.client.id };
+    await updateClient(args, context);
+    return await getClient(args, context);
   },
 };
 
@@ -86,7 +186,6 @@ app.use(
     rootValue,
     graphiql: true,
     context: {
-      test: 'hello world',
       db,
     },
   }))
